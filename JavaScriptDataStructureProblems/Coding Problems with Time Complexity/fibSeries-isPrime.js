1270c745a06f320e15720c25ab1d5e52a48baa41
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-08 19:29:53 -0700

    updated with recursion

--- JavaScriptDataStructureProblems/Coding Problems with Time Complexity/fibSeries-isPrime.js
+++ JavaScriptDataStructureProblems/Coding Problems with Time Complexity/fibSeries-isPrime.js
@@ -1,8 +1,10 @@
+    // Calculating the sum of array 
+
     function sum(n){
-    let sum=0;
-    for(key in n){
-    sum+=n[key];
-    }  
+      let sum=0;
+      for(key in n){
+        sum+=n[key];
+      }  
     return sum;
     }
 
@@ -22,8 +24,21 @@
     // 3+n-1
     // 3+1*(n-1)=  n-1= n BigO= n
 
+    function fibByRec(n){
+       if(n===0 ||n===1){
+          return 1;
+       }
+      return fib(n-1)+fib(n-2);
+    }
+    console.log("fib by recursion value"+" "+fibByRec(3));
+// It has excecution then O(n) it grows in exponention way O(2^n)
+//T=> O(2^n) it is far slower then O(n)=> O(n*n)
+
+    
+// checkiing if a given number is prime or not by using Math.sqrt function
      function isPrime(n){
-        for(let i=2;i<Math.sqrt(n);i++){
+        for(let i=2;i<Math.sqrt(n);i++){ 
+   // i<n is also correct but it will increase the complexity by O(n)
             if(n % i===0 ) {return false;}   
         }
         return true;
@@ -35,12 +50,12 @@
      //worst case n=n-2 => O(n) 
 
      function minNum(n){
-        return Math.min(...n);
+        return Math.min(...n); 
      }
      const n =[1,9,2,4,1];
      //console.log(minNum(n));
 
-     
+     // star pattern problem
      for(var i=1; i<=4; i++){
         console.log("*".repeat(i));
      }
