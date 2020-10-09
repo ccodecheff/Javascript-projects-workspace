ad69559ec6659180c449fee0dc7dea1f614fb32f
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-08 19:39:35 -0700

    Updated with recursion

--- JavaScriptDataStructureProblems/Coding Problems with Time Complexity/fibSeries-isPrime.js
+++ JavaScriptDataStructureProblems/Coding Problems with Time Complexity/fibSeries-isPrime.js
@@ -23,19 +23,38 @@
     // time complexity = 1+1+1+n-1
     // 3+n-1
     // 3+1*(n-1)=  n-1= n BigO= n
+// Fib by recursion 
 
+    let counter=0;
     function fibByRec(n){
+       counter++;
        if(n===0 ||n===1){
           return 1;
        }
-      return fib(n-1)+fib(n-2);
+      return (fibByRec(n-1)+fibByRec(n-2));
     }
-    console.log("fib by recursion value"+" "+fibByRec(3));
+    console.log("fib by recursion value"+" "+fibByRec(4));
+// Testing fib counter by recursive function
+    fibByRec(5);
+    console.log(counter);
+    count=0;
+
+    fibByRec(6);
+    console.log(counter);
+    count=0;
+
+    fibByRec(7);
+    console.log(counter);
+    count=0;
+
+    fibByRec(8);
+    console.log(count);
+    count=0;
 // It has excecution then O(n) it grows in exponention way O(2^n)
 //T=> O(2^n) it is far slower then O(n)=> O(n*n)
-
     
-// checkiing if a given number is prime or not by using Math.sqrt function
+
+// checking if a given number is prime or not by using Math.sqrt function
      function isPrime(n){
         for(let i=2;i<Math.sqrt(n);i++){ 
    // i<n is also correct but it will increase the complexity by O(n)
