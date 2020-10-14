102df2c6a677e27b4e037459cce21ffcbddcd27e
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-13 21:31:38 -0700

    updated array methods

--- JavaScriptPracticeProblems/arrayMethods.js
+++ JavaScriptPracticeProblems/arrayMethods.js
@@ -63,7 +63,52 @@ arr.forEach(function(item,index,array){
             {id:1,name:"karan"},
             {id:2,name:"nish"}
             ];
-    let user = users.find(item=>item.id===2);
-    console.log(user.name);
+    let user = users.find(item=>item.id>0);
+    console.log(user); // it will stop once the condition is true
+
+    // filter(callback item, index, array) - it is used to find multiple elements
+    user = users.filter(item=>item.id>0);
+    console.log(user); // two users
+
+/**************************************************/
+//    map,sort,split and join
+//************************************************ */ 
+//It calls the function for each element of the array and returns the array of results.
+     user = users.map(item=>item.name.length);
+     console.log(user); // length of karan and nish
+
+//sort 
+    let stringarr = ["a","b","c"];
+    console.log(stringarr.sort()); 
+ // by default it sort only string, for numbers ot converts it into string then sort it
+// for example 
+    numberarr = [10,3,-5,8];
+    console.log(numberarr.sort()); 
+// for numbers it requires a callback function
+    console.log(numberarr.sort(
+        (a,b)=>a-b  // Aesc order
+    ));
+
+    console.log(numberarr.sort(
+        (a,b)=>b-a  // desc order
+    )); 
+
+// reverse 
+    console.log(numberarr.reverse()); //[10, 8, 3, -5] => [-5, 3, 8, 10]
+
+/**************************************************/
+//    reduce or reduceRight - it returns a result value based on the array
+//  let value = arr.reduce(function(result, item, index, array) {
+//     // ...
+//   }, [initial value]);
+//************************************************ */ 
+
+console.log("calculate of some of these arrays:"+numberarr);
+
+    // 0 is the initial value of sum , item is the array element 
+    let sum = numberarr.reduce((sum,item)=>sum+item,0); 
+    console.log(sum);
+
+    
 
 
