0a4f059fc2dccd55e54e3cb2bcff764f27b602ee
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-19 22:28:23 -0700

    minor changes

--- JavaScriptPracticeProblems/stairPattern.js
+++ JavaScriptPracticeProblems/stairPattern.js
@@ -1,14 +1,14 @@
-// program to create stair pattern 
+// program to create stair pattern where last line is not preceded with space
 //     *
 //    **
 //   ***
 //  ****    
 
 let n =6;
-let i=0;
+let i=1;
 let j=n;
-while(i<n && j>0 ){ 
-   console.log(" ".repeat(j-1)+"*".repeat(i)+"\n");
+while(i<=n && j>=0 ){ 
+   console.log("* ".repeat(j-1)+"#".repeat(i)); 
   // console.log("*".repeat(i)+"\n");
   i++;
   j--;
