4a853c582eaf945285a6eaff2dec1ac77e7d9050
ccodecheff <rathore.karan.sn1@gmail.com>
2020-10-25 21:40:55 -0700

    update

--- JavaScriptPracticeProblems/stairPattern.js
+++ JavaScriptPracticeProblems/stairPattern.js
@@ -4,12 +4,13 @@
 //   ***
 //  ****    
 
-let n =6;
+let n =10;
 let i=1;
 let j=n;
 while(i<=n && j>=0 ){ 
-   console.log("* ".repeat(j-1)+"#".repeat(i)); 
+  console.log("* ".repeat(j-1)+"#".repeat(i)); 
   // console.log("*".repeat(i)+"\n");
+  
   i++;
   j--;
 }
