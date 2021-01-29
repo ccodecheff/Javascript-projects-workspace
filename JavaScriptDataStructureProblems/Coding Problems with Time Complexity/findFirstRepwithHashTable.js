6544381277743cd5e4d4cfb8d5c47d823b347829
ccodecheff <rathore.karan.sn1@gmail.com>
2021-01-28 21:18:48 -0800

    update hash table

---
+++ JavaScriptDataStructureProblems/Coding Problems with Time Complexity/findFirstRepwithHashTable.js
@@ -0,0 +1,13 @@
+const word = 'Logical mind';
+
+function findFirstRep(word){
+const table={};
+for(const char of word ){
+    if(table[char]){
+        return char;
+    }
+    // assigning key 1 to every character 
+    table[char]=1;
+}
+}
+console.log(findFirstRep(word));
